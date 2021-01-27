8d1722ef7bce2fd0d573cbd67ee2d02e5f87145a
Artyom Fomenko <fantastik.play.2000@gmail.com>
2021-01-27 17:37:51 +0300

    *

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <img src="/img/on.jpg" alt="" />
+  </div>
+</template>
+<style>
+div {
+  background-color: black;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <img src="/img/off.jpg" alt="" />
+  </div>
+</template>
+
+<style>
+div {
+  background-color: black;
+}
+</style>
